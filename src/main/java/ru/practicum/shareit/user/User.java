ae1acabd08ac519238ffa772fcd4dacf499d64b7
Андрей Скоробогатов <ufo1og@yandex.ru>
2023-01-25 14:36:28 +0500

    feat: Написать UNIT-тесты для класса UserServiceImpl.

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.user;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Setter
@@ -20,4 +21,17 @@ public class User {
     private String name;
     @Column(name = " email", nullable = false)
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
 }
